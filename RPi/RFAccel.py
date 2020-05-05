dfe027376fc951cd3db214765da44e6893eca89e
Cederik Höfs <cederik_hoefs@t-online.de>
2020-05-05 20:58:41 +0200

    First test

---
+++ RPi/RFAccel.py
@@ -0,0 +1,15 @@
+cmd =    0x00
+ack =    0x01
+data =   0x02
+stream = 0x03
+
+cmd_who_am_i = 0x00
+cmd_start = 0x01
+cmd_end = 0xFF
+cmd_set_acc_rate = 0x02
+cmd_set_gyr_rate = 0x03
+cmd_set_acc_range = 0x05
+cmd_set_gyr_range = 0x06
+cmd_get_acc = 0x07
+cmd_get_gyr = 0x08
+
---
+++ RPi/main.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+import time
+from RF24 import *
+from RFAccel import *
+import RPi.GPIO as GPIO
+
+irq_gpio_pin = None
+
+millis = lambda: int(round(time.time() * 1000))
+
+
+
+########### USER CONFIGURATION ###########
+# See https://github.com/TMRh20/RF24/blob/master/pyRF24/readme.md
+
+# CE Pin, CSN Pin, SPI Speed
+
+# Setup for GPIO 22 CE and CE0 CSN with SPI Speed @ 8Mhz
+radio = RF24(RPI_V2_GPIO_P1_15, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_8MHZ)
+
+if __name__ == "__main__":
+    main()
+
+
+def main():
+
+	print("RFAccel 0.1")
+
+	pipes = [0xF0F0F0F0E1, 0xF0F0F0F0D2]
+
+	radio.begin()
+	radio.enableDynamicPayloads()
+	radio.setRetries(5, 15)
+	radio.printDetails()
+
+	radio.openWritingPipe(pipes[0])
+	radio.openReadingPipe(1, pipes[1])
+
+	radio.stopListening()
+	radio.write([cmd, cmd_start])
+
+	radio.startListening()
+
+	wait_start = millis()
+	timeout = False
+	while (not radio.available()) and (not timeout):
+		if (millis() - wait_start) > 500:
+			timeout = True
+
+	if timeout:
+		print('Connection failed, response timed out.')
+	else:
+		length = radio.getDynamicPayloadSize()
+		response = radio.read(length)
+
+		print("Received " + str(length) + " bytes")
