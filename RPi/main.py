4db85d4d2cf6d04662415e005ea78338b76bfaf2
Cederik Höfs <cederik_hoefs@t-online.de>
2020-05-19 18:56:22 +0200

    Forgot to stopListening on RPi

--- RPi/main.py
+++ RPi/main.py
@@ -251,7 +251,7 @@ class RFAccelShell(cmd.Cmd):
 
 					if((r_type == RFAccel.type_cmd) and (r_cmd == RFAccel.cmd_test_channel)):
 						print("Received test channel command on new channel.")
-						self.radio.printDetails()
+						self.radio.stopListening()
 						self.radio.write(bytearray([RFAccel.type_cmd, RFAccel.cmd_test_channel]))
 						print("Sent test channel command on new channel.")
 
